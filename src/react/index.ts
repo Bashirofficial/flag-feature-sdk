1caf6b5ac73c1cd98cc51d33147ee5407e59231a
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2026-03-27 15:31:22 +0530

    fix: useFeatureFlagClient is being imported from provider

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,6 +1,5 @@
-export { FeatureFlagProvider } from "./provider";
+export { FeatureFlagProvider, useFeatureFlagClient } from "./provider";
 
-export { useFeatureFlagClient } from "./hooks/useClient";
 export { useFeatureFlag } from "./hooks/useFlag";
 export { useStringFlag } from "./hooks/useStringFlag";
 export { useNumberFlag } from "./hooks/useNumberFlag";
